6e4ff95abd82ccfb9e80be73f6a8cc208e10a35e
V ranjith <ranjithsvelmurugan@gmail.com>
2023-07-31 14:19:02 +0530

    login page ver 1.0

--- humcen_user/client/components/Authentication/SignInForm.js
+++ humcen_user/client/components/Authentication/SignInForm.js
@@ -74,154 +74,150 @@ const SignInForm = () => {
   return (
     <>
       <div className="authenticationBox">
-        <Box
-          component="main"
-          sx={{
-            maxWidth: "510px",
-            ml: "auto",
-            mr: "auto",
-            padding: "50px 0 100px",
-          }}
-        >
-          <Grid item xs={12} md={12} lg={12} xl={12}>
-            <Box>
-              <Typography as="h1" fontSize="28px" fontWeight="700" mb="5px">
-                <img
-                  src="/images/logo.png"
-                  alt="favicon"
-                  className={styles.favicon}
-                />
-              </Typography>
-
-              <Typography fontSize="15px" mb="30px">
-                Don't have an account?{" "}
-                <Link
-                  href="/authentication/sign-up"
-                  className="primaryColor text-decoration-none"
-                >
-                  Sign up
-                </Link>
-              </Typography>
-
-
-            
-              <Box component="form" noValidate onSubmit={handleSubmit}>
-                <Box
-                  sx={{
-                    background: "#fff",
-                    padding: "30px 20px",
-                    borderRadius: "10px",
-                    mb: "20px",
-                  }}
-                  className="bg-black"
-                >
-                  <Grid container alignItems="center" spacing={2}>
-                    <Grid item xs={12}>
-                      <Typography
-                        component="label"
-                        sx={{
-                          fontWeight: "500",
-                          fontSize: "14px",
-                          mb: "10px",
-                          display: "block",
-                        }}
-                      >
-                        Email
-                      </Typography>
-
-                      <TextField
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        InputProps={{
-                          style: { borderRadius: 8 },
-                        }}
-                      />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                      <Typography
-                        component="label"
-                        sx={{
-                          fontWeight: "500",
-                          fontSize: "14px",
-                          mb: "10px",
-                          display: "block",
-                        }}
-                      >
-                        Password
-                      </Typography>
-
-                      <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                        InputProps={{
-                          style: { borderRadius: 8 },
-                        }}
-                      />
-                    </Grid>
-                  </Grid>
-                </Box>
-
-                {error && (
-                  <Typography
-                    variant="body2"
-                    color="error"
-                    align="center"
-                    sx={{ mb: 2 }}
+      <div className={styles.container}>
+          <div className={styles.leftContainer}>
+            <div className={styles.topContainer}>
+              {/* Content for the top container */}
+            </div>
+            <div className={styles.bottomContainer}>
+                <Typography as="h1" fontSize="28px" fontWeight="700" mb="5px">
+                  <img
+                    src="/images/logo-white.png"
+                    alt="favicon"
+                    className={styles.favicon}
+                  />
+                </Typography>
+            </div>
+          </div>
+          <div className={styles.rightContainer}>
+              <Box>
+                <h1>Login Your account</h1>                      
+                <Box component="form" noValidate onSubmit={handleSubmit}>
+                  <Box
+                    sx={{
+                      background: "#fff",
+                      padding: "30px 20px",
+                      borderRadius: "10px",
+                      mb: "20px",
+                    }}
+                    className="bg-black"
                   >
-                    {error}
-                  </Typography>
-                )}
+                    <Grid container alignItems="center" spacing={2}>
+                      <Grid item xs={12}>
+                        <Typography
+                          component="label"
+                          sx={{
+                            fontWeight: "500",
+                            fontSize: "14px",
+                            mb: "10px",
+                            display: "block",
+                          }}
+                        >
+                          Email
+                        </Typography>
 
-                <Grid container alignItems="center" spacing={2}>
-                  <Grid item xs={6} sm={6}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox value="allowExtraEmails" color="primary" />
-                      }
-                      label="Remember me."
-                    />
-                  </Grid>
+                        <TextField
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          InputProps={{
+                            style: { borderRadius: 8 },
+                          }}
+                        />
+                      </Grid>
 
-                  <Grid item xs={6} sm={6} textAlign="end">
-                    <Link
-                      href="/authentication/forgot-password"
-                      className="primaryColor text-decoration-none"
+                      <Grid item xs={12}>
+                        <Typography
+                          component="label"
+                          sx={{
+                            fontWeight: "500",
+                            fontSize: "14px",
+                            mb: "10px",
+                            display: "block",
+                          }}
+                        >
+                          Password
+                        </Typography>
+
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          InputProps={{
+                            style: { borderRadius: 8 },
+                          }}
+                        />
+                      </Grid>
+                    </Grid>
+                  </Box>
+
+                  {error && (
+                    <Typography
+                      variant="body2"
+                      color="error"
+                      align="center"
+                      sx={{ mb: 2 }}
                     >
-                      Forgot your password?
-                    </Link>
-                  </Grid>
-                </Grid>
+                      {error}
+                    </Typography>
+                  )}
 
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{
-                    mt: 2,
-                    textTransform: "capitalize",
-                    borderRadius: "8px",
-                    fontWeight: "500",
-                    fontSize: "16px",
-                    padding: "12px 10px",
-                    color: "#fff !important",
-                  }}
-                >
-                  Sign In
-                </Button>
+                  <Grid container alignItems="center" spacing={2}>
+                    <Grid item xs={6} sm={6}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox value="allowExtraEmails" color="primary" />
+                        }
+                        label="Remember me."
+                      />
+                    </Grid>
+
+                    <Grid item xs={6} sm={6} textAlign="end">
+                      <Link
+                        href="/authentication/forgot-password"
+                        className="primaryColor text-decoration-none"
+                      >
+                        Forgot your password?
+                      </Link>
+                    </Grid>
+                  </Grid>
+
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{
+                      mt: 2,
+                      textTransform: "capitalize",
+                      borderRadius: "8px",
+                      fontWeight: "500",
+                      fontSize: "16px",
+                      padding: "12px 10px",
+                      color: "#fff !important",
+                    }}
+                  >
+                    Sign In
+                  </Button>
+                  <Typography fontSize="15px" mb="30px">
+                  Don't have an account?{" "}
+                  <Link
+                    href="/authentication/sign-up"
+                    className="primaryColor text-decoration-none"
+                  >
+                    Sign up
+                  </Link>
+                </Typography>
+                </Box>
               </Box>
-            </Box>
-          </Grid>
-        </Box>
+          </div>
+        </div>
       </div>
     </>
   );
