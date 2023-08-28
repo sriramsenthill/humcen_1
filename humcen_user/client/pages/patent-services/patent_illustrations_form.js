82680d4e05993d4499d0ce61203d49a76ca2b083
Senthilnathan S <macbook@Senthilnathans-MacBook-Air.local>
2023-08-28 17:38:28 +0530

    changed all forms frontend

--- humcen_user/client/pages/patent-services/patent_illustrations_form.js
+++ humcen_user/client/pages/patent-services/patent_illustrations_form.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import BannerCard from "@/components/BannerCard";
 import Link from "next/link";
 import style from "@/styles/PageTitle.module.css";
-import { Button, ButtonProps, Card, InputLabel } from "@mui/material";
+import { Button, ButtonProps, Card } from "@mui/material";
 import { styled } from "@mui/system";
 import DefaultSelect from "@/components/Forms/AdvancedElements/DefaultField";
 import { Typography } from "@mui/material";
@@ -17,6 +17,15 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import OkDialogueBox from "./dialoguebox";
 import CustomDropZone from "@/components/CustomDropBox";
+import Head from "next/head";
+import {
+  Container,
+  Paper,
+
+  Divider,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
@@ -30,31 +39,36 @@ api.interceptors.request.use((config) => {
 });
 
 
-const ColorButton = styled(Button)(({ theme }) => ({
-  color: "white",
-  width: "120%",
-  height: "52px",
-  borderRadius: "100px",
-  marginBottom: "30px",
-  background: "linear-gradient(270deg, #02E1B9 0%, #00ACF6 100%)",
-  "&:hover": {
-    background: "linear-gradient(270deg, #02E1B9 0%, #00ACF6 100%)",
-  },
-  textTransform: "none",
-  fontSize: "14px",
-  fontWeight: "400",
-}));
-
 export default function Inbox() {
-  const [domain, setDomain] = useState("");
-  const [country, setCountry] = useState("");
+    const [draftingOpen, setDraftingOpen] = useState(true);
+    const [countriesOpen, setCountriesOpen] = useState(false);
+    const [contactOpen, setContactOpen] = useState(false);
+    const [domain, setDomain] = useState("");
+    const [country, setCountry] = useState("");
+    const [title, setTitle] = useState("");
+    const [detailsFile, setDetailsFile] = useState(null);
+    const [time, setTime] = useState("");
+    const [budget, setBudget] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isErrorDialogOpen, setIsErrorDialogOpen] = useState(false);
+    const [isErrorDialogOpenStatus, setIsErrorDialogOpenStatus] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [summary, setSummary] = useState([]);
+  const [totalBill, setBill] = useState([]); // Bill amount state
+  const [shoppingList, setList] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+  const [description, setDescription] = useState(null);
+  const [patentDetails, setPatentDetails] = useState(null);
+  const [files, setFiles] = useState(null);
+  const [businessObj,setBusinessObj]=useState('');
+  const [marketAndIndustry,setMarketAndIndustry]=useState('');
+  const [sourceLanguage, setSourceLanguage] = useState("");
+  const [targetLanguage, setTargetLanguage] = useState("");
+  const [additionalInfo,setAdditionalInfo]=useState("")
   const [patentSpe, setPatentSpe] = useState("");
   const [drawingReq, setdrawingReq] = useState("");
   const [preferredStyleFile, setpreferredStyleFile] = useState(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [isErrorDialogOpen, setIsErrorDialogOpen] = useState(false);
-  const [isErrorDialogOpenStatus, setIsErrorDialogOpenStatus] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+
   const router = useRouter();
 
 
@@ -117,196 +131,84 @@ export default function Inbox() {
     router.push("/");
   };
 
+  const handleDraftingContinue = () => {
+    setDraftingOpen(false);
+    setCountriesOpen(true);
+  };
+
+
+
+  const handleCountriesContinue = () => {
+    setCountriesOpen(false);
+    setContactOpen(true);
+
+    setSummary([
+      {
+        title: "Title",
+        text: title,
+      },
+      {
+        title: "Domain",
+        text: domain,
+      },
+      {
+        title: "Keywords",
+        text: keywords.toString()
+      },
+      {
+        title: "Uploaded Files",
+        text: [detailsFile.map((file) => file.name)].toString()
+      }
+    ]);
+
+    for(let choices=0; choices < country.length; choices++){
+      setList(shoppingList => [...shoppingList, {
+        country: country[choices],
+        cost: totalBill[choices]
+      }])
+    }
+  };
+
 
   return (
     <>
-      <div className="card">
-      <div className={style.pageTitle}>
+      <div style={{ margin: '0 1rem' }}>
+        <Paper elevation={3} style={{ borderRadius: '16px', padding: '1rem', margin: '1rem 0' }}>
+          {/* Banner */}
 
-        <ul>
-          <li>
-            <Link href="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/patent-services">My Patent Services</Link>
-          </li>
-          <li>Patent Illustration Form</li>
-        </ul>
-      </div>
-      <BannerCard title="Patent Illustration" imageSrc="/images/banner_img/bg.png" color="white"></BannerCard>
-      <div className={style.ccard}>
-      {/* <h1 className={`${style.heading} ${style.align}`}>Patent Illustration Service</h1>
 
-      <p
-        style={{
-          fontFamily: "Inter",
-          fontStyle: "normal",
-          fontWeight: "400",
-          fontSize: "16px",
-          lineHeight: "19px",
-          paddingLeft: "12%",
-          marginTop: "8px"
-        }}
-      >
-        Let's get started with the basic details for Patent Illustration Service
-      </p> */}
-      <Card variant="outlined" sx={{ marginLeft: "2%", marginRight: "2%", width: "95%", borderRadius: "15px" }}>
-    
-      <form onSubmit={handleSubmit}>
-      <Card variant="outlined" sx={{ margin: "5% 12%" }}>
-        <DefaultSelect domain={domain} onDomainChange={handleDomainChange} />
-        <Card
-            sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
-              mb: "10px",
-            }}
-          >
-            <Typography
-              as="h3"
-              sx={{
-                fontSize: 18,
-                fontWeight: 500,
-                mb: "10px",
-              }}
-            >
-              Target Country :
-            </Typography>
-            <Typography
-              as="h5"
-              sx={{
-                fontSize: 12,
-                fontWeight: 350,
-                mb: "10px",
-              }}
-            >
-             ( Specify the country or countries where you primarily seek to protect and enforce your patent illustration. )
-            </Typography>
-            <Button
-              style={{
-                background: country === "India" ? "#68BDFD" : "#F8FCFF",
-                color: country === "India" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("India"); // Update the country state on button click
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/in.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;India
-            </Button>
-            <Button
-              style={{
-                background: country === "United States" ? "#68BDFD" : "#F8FCFF",
-                color: country === "United States" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("United States");
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/us.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;United States
-            </Button>
-            <Button
-              style={{
-                background: country === "Germany" ? "#68BDFD" : "#F8FCFF",
-                color: country === "Germany" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("Germany");
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/de.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;Germany
-            </Button>
-            <Button
-              style={{
-                background: country === "China" ? "#68BDFD" : "#F8FCFF",
-                color: country === "China" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("China");
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/cn.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;China
-            </Button>
-            <Button
-              style={{
-                background: country === "UAE" ? "#68BDFD" : "#F8FCFF",
-                color: country === "UAE" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("UAE");
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/ae.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;UAE
-            </Button>
-            <Button
-              style={{
-                background: country === "Japan" ? "#68BDFD" : "#F8FCFF",
-                color: country === "Japan" ? "white" : "#BFBFBF",
-                width: "13%",
-                marginRight: "2%",
-                height: "40px",
-                textTransform: "none",
-              }}
-              onClick={() => {
-                setCountry("Japan");
-              }}
-            >
-              <img
-                src="https://hatscripts.github.io/circle-flags/flags/jp.svg"
-                width="24"
-              />
-              &nbsp;&nbsp;Japan
-            </Button>
-          </Card>
-          <Card
-            sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
-              mb: "10px",
-            }}
-          >
-            <Typography
+    {/* Page title */}
+    <div className={style.pageTitle}>
+  <ul>
+    <li>
+      <Link href="/">Dashboard</Link>
+    </li>
+    <li>
+      <Link href="/patent-services">My Patent Services</Link>
+    </li>
+    <li>Patent Illustration</li>
+  </ul>
+</div>
+
+    <Container maxWidth="md" style={{ marginTop: '2rem' }}>
+      <Head>
+        <title>Patent Illustration</title>
+      </Head>
+      <BannerCard
+  title="Patent Illustration"
+  imageSrc="/images/banner_img/bg.png"
+  color="white"
+  style={{ width: '100%', maxWidth: '1200px', margin: '550%' }}></BannerCard>
+
+      <Typography variant="h5" onClick={() => setDraftingOpen(!draftingOpen)} style={{ cursor: 'pointer' }}>
+      Patent Illustration
+      <ExpandMoreIcon style={{ transform: draftingOpen ? 'rotate(180deg)' : 'rotate(0deg)' }} />
+      </Typography>
+
+      {draftingOpen && (
+        <div style={{ padding: '1rem 0' }}>
+          <DefaultSelect domain={domain} onDomainChange={handleDomainChange}/>
+          <Typography
               as="h3"
               sx={{
                 fontSize: 18,
@@ -328,16 +230,7 @@ export default function Inbox() {
           
               onChange={patentSpecifications} // Provide the onChange event handler
             />
-          </Card>
-          <Card
-            sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
-              mb: "10px",
-            }}
-          >
-            <Typography
+       <Typography
               as="h3"
               sx={{
                 fontSize: 18,
@@ -359,16 +252,7 @@ export default function Inbox() {
           
               onChange={drawingRequirements} // Provide the onChange event handler
             />
-          </Card>
-          <Card
-            sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
-              mb: "10px",
-            }}
-          >
-            <Typography
+   <Typography
               as="h3"
               sx={{
                 fontSize: 18,
@@ -389,66 +273,108 @@ export default function Inbox() {
              ( If you have a preferred illustration style or any specific examples or references, please share them with us. )
             </Typography>
             <CustomDropZone files={preferredStyleFile} onFileChange={getFiles}/>
-          </Card>
-          
-          <Card
+
+         
+            
+          <Button variant="contained" color="primary" onClick={handleDraftingContinue} style={{ marginTop: '1rem' }}>
+            Continue
+          </Button>
+        </div>
+      )}
+      <Divider style={{ margin: '2rem 0' }} />
+      <Typography variant="h5">Target Country</Typography>
+      {countriesOpen && (
+        <div style={{ padding: '1rem 0' }}>
+          {/* Country Selection Buttons */}
+          <Typography
+            as="h3"
             sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
+              fontSize: 18,
+              fontWeight: 500,
               mb: "10px",
             }}
           >
-            <CheckBox /> I have read and agreed to the following policies -
-            Humcen Privacy Policy, Humcen Terms & Conditions, before proceeding.
-          </Card>
-          <Card
-            sx={{
-              boxShadow: "none",
-              borderRadius: "10px",
-              p: "25px",
-              mb: "10px",
+            Select the Country
+          </Typography>
+          <Button
+            style={{
+              background: country.includes("India") ? "#68BDFD" : "#F8FCFF",
+              color: country.includes("India") ? "white" : "#BFBFBF",
+              width: "15%",
+              marginRight: "2%",
+              height: "60px",
+              textTransform: "none",
+            }}
+            onClick={() => {
+              if(!country.includes("India")) {
+                setCountry(country => [...country, "India"]);
+                setBill([...totalBill, 625]);
+              } else {
+                setCountry(country.filter(nation => nation != "India"));
+                setBill(totalBill.filter(bill => bill != 625));
+              }
+              console.log(country);
+              console.log(totalBill);
             }}
           >
-            <Link
-              href="/patent-services/drafting-form"
-              style={{ textDecoration: "none" }}
-            >
-              <ColorButton
-                sx={{ width: "15%" }}
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Submit
-              </ColorButton>
-            </Link>
-          </Card>
-        </Card>
-      </form>
-      </Card>
+            <img
+              src="https://hatscripts.github.io/circle-flags/flags/in.svg"
+              width="24"
+            />
+            &nbsp;&nbsp;India <br />&nbsp;&nbsp;&#40;&#36;625&#41;
+          </Button>
+          <Button
+            style={{
+              background: country.includes("United States") ? "#68BDFD" : "#F8FCFF",
+              color: country.includes("United States") ? "white" : "#BFBFBF",
+              width: "18%",
+              marginRight: "2%",
+              height: "60px",
+              textTransform: "none",
+            }}
+            onClick={() => {
+              if(!country.includes("United States")) {
+                setCountry(country => [...country, "United States"]);
+                setBill([...totalBill, 900]);
+              } else {
+                setCountry(country.filter(nation => nation != "United States"));
+                setBill(totalBill.filter(bill => bill != 900))
+              }
+              console.log(country);
+              console.log(totalBill);
+            }}
+            value="United States"
+          >
+            <img
+              src="https://hatscripts.github.io/circle-flags/flags/us.svg"
+              width="24"
+            />
+            &nbsp;&nbsp;United States <br />&nbsp;&nbsp;&#40;&#36;900&#41;
+          </Button>
+          {/* Add other country buttons similarly */}
+          <Button variant="contained" color="primary" onClick={handleCountriesContinue} style={{ marginTop: '1rem' }}>
+            Continue
+          </Button>
+        </div>
+      )}
 
-      {isSubmitted && <OkDialogueBox domainValue={domain} serviceValue={"Patent Illustration"}/> }
-      <Dialog open={isErrorDialogOpen}>
-    <DialogTitle>Error</DialogTitle>
-    <DialogContent>
-      <p>Please fill all the required details before submitting the form.</p>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={() => setIsErrorDialogOpen(false)}>OK</Button>
-    </DialogActions>
-  </Dialog>
-  <Dialog open={isErrorDialogOpenStatus}>
-  <DialogTitle>Error</DialogTitle>
-  <DialogContent>
-    <p>{errorMessage}</p>
-  </DialogContent>
-  <DialogActions>
-    <Button onClick={() => setIsErrorDialogOpenStatus(false)}>OK</Button>
-  </DialogActions>
-</Dialog>
+      <Divider style={{ margin: '2rem 0' }} />
+      <Typography variant="h5">Summary</Typography>
+      { contactOpen && <div style={{ padding: '1rem 0' }}>
+          {/* Your content for the 'Contact' section */}
+          <ShoppingCart priceList={shoppingList} detailsList={summary} total={totalBill.reduce((a,b)=> a+b,0)}service="Patent Drafting"/>
+          <Button variant="contained" onClick={() => handleSubmit()} style={{ marginTop: '0.5rem', backgroundColor: "#00B69B" }}>
+            Submit
+        </Button>
+        </div>
+        }
 
-      </div>
-      </div>
+
+    </Container>
+    </Paper>
+    </div>
+
     </>
   );
-}
+};
+
